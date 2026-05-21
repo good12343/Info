8556962b8397c439f0bafc2facb382e6de61f3a9
good12343 <abrehymalqedy66@gmail.com>
2026-05-21 04:52:59 +0000

    important

--- k/src/config/contracts.ts
+++ k/src/config/contracts.ts
@@ -2,7 +2,7 @@ export const contracts = {
   sepolia: {
     chainId: 11155111,
     name: "Sepolia",
-    rpc: process.env.SEPOLIA_RPC || "https://rpc.sepolia.org",
+    rpc: process.env.SEPOLIA_RPC || "https://ethereum-sepolia.core.chainstack.com/892a28be8ff030c1544ff6eaf9c6ef0f",
     token: process.env.TOKEN_ADDRESS || "0x0000000000000000000000000000000000000000",
     airdrop: process.env.AIRDROP_ADDRESS || "0x0000000000000000000000000000000000000000",
     vesting: process.env.VESTING_ADDRESS || "0x0000000000000000000000000000000000000000",
